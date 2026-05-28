5abf5a41c68f4e51f648804d2b47c000082c2006
alyasabrinaya-ui <alyasabrinaya@gmail.com>
2026-05-29 05:39:02 +0700

    Add files via upload

---
+++ modul-4/modul-4/soal 1.sql
@@ -0,0 +1,8 @@
+SELECT 
+    nama_mk,
+    sks,soal
+    nama_dosen
+FROM mata_kuliah AS mk
+JOIN dosen AS d 
+    ON mk.id_dosen = d.id_dosen
+WHERE d.bidang_keahlian IN ('Pemrograman', 'Basis Data');
---
+++ modul-4/modul-4/soal 2.sql
@@ -0,0 +1,6 @@
+SELECT 
+   nama_mahasiswa,
+    k.id_mata_kuliah AS id_mk
+FROM mahasiswa AS m
+LEFT JOIN krs AS k
+    ON m.id_mahasiswa = k.id_mahasiswa;
